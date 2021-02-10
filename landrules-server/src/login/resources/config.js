9f85f6f54a40f25862b98240b68bf8d8de4a529c
emmaguo13 <emmaguo@berkeley.edu>
2021-02-09 22:39:09 -0800

    login files

---
+++ landrules-server/src/login/resources/config.js
@@ -0,0 +1,11 @@
+
+module.exports =
+    config = {
+        typingDNA: {
+            apiKey: 'Your ApiKey',
+            apiSecret: 'Your ApiSecret',
+            apiServer: 'api.typingdna.com',
+        },
+        someprivatekey: '@2mws9~&%?+RN',
+        sessionSecret: 'asdkjkfh63ryfsdgcqg87w4rf7gasydi',
+    };
