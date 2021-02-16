7d299f665b530dd0157590072025b9958eac2f6a
emmaguo13 <emmaguo@berkeley.edu>
2021-02-15 22:44:38 -0800

    form

--- landrules-react/src/pages/Form.js
+++ landrules-react/src/pages/Form.js
@@ -1,23 +1,50 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Button, Tabs, Input, Upload, message } from 'antd';
+import { Button, Tabs, Input, Upload, message} from 'antd';
 import Login from './Login.js';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 
 import MapPicker from '../components/MapPicker';
 
+const { TextArea } = Input;
+
 function Form() {
-    const [name, setName] = useState('');
-    const [address, setAddress] = useState('');
+    const [date, setDate] = useState('')
+    const [deedName, setDeedName] = useState('')
+    const [granteeName, setGranteeName] = useState('')
+    const [address, setAddress] = useState('')
+    const [granteePhone, setGranteePhone] = useState('')
+    const [propertyDescription, setPropertyDescription] = useState('')
     const [metamaskAddress, setMetamaskAddress] = useState('');
     const [toLogin, setToLogin] = useState(false);
-    const [role, setRole] = useState('');
     const [points, setPoints] = useState([]);
 
     function handleSubmit() {
         // setToLogin(true);
+        let json = {
+            date: date,
+            deedName: deedName,
+            granteeName: granteeName,
+            granteePhone: granteePhone,
+            address: address,
+            propertyDescription: propertyDescription,
+
+            
+        }
         console.log(process.env.REACT_APP_SERVER_URL);
-        axios.post(`${process.env.SERVER_UR}/api/deed/`, {});
+        fetch(`${process.env.SERVER_UR}/api/deed/newDeed`, {
+        body: JSON.stringify({
+            name: deedName,
+            comments: propertyDescription,
+            coordinates: points,
+            address: address,
+            json: json,
+        }),
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "POST",
+        }).then((response) => response.json());
     }
 
     return (
@@ -29,7 +56,7 @@ function Form() {
                 <div
                     style={{
                         width: '100vw',
-                        height: '100vh',
+                        height: '105vh',
                         backgroundColor: '#E9FFFA',
                         display: 'flex',
                         flexDirection: 'column',
@@ -49,29 +76,83 @@ function Form() {
                         }}
                     >
                         <div
+                        style={{
+                            
+                            zIndex: '0',
+                        
+                        }}>
+
+                        
+                        <div
                             style={{
+                                position: 'absolute',
+                                zIndex: '-1',
+                                top:'7%',
+                                left:'7%'
+                            
+                            }}
+                        >
+                            <svg width="500" height="700" viewBox="0 0 702 709" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <g filter="url(#filter0_bd)">
+                            <rect x="4.58215" y="0.589722" width="693.418" height="766.41" rx="51" fill="white"/>
+                            </g>
+                            <defs>
+                            <filter id="filter0_bd" x="0.582153" y="-3.41028" width="701.418" height="778.41" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                            <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                            <feGaussianBlur in="BackgroundImage" stdDeviation="2"/>
+                            <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur"/>
+                            <feColorMatrix in="SourceAlpha" type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 127 0"/>
+                            <feOffset dy="4"/>
+                            <feGaussianBlur stdDeviation="2"/>
+                            <feColorMatrix type="matrix" values="0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0.25 0"/>
+                            <feBlend mode="normal" in2="effect1_backgroundBlur" result="effect2_dropShadow"/>
+                            <feBlend mode="normal" in="SourceGraphic" in2="effect2_dropShadow" result="shape"/>
+                            </filter>
+                            </defs>
+                            </svg>
+                        </div>
+                        <div
+                            style={{
+                                
+                                zIndex: '1',
                                 display: 'flex',
                                 alignItems: 'center',
                                 flexDirection: 'column',
-                                width: '45vw',
+                                width: '30vw',
                                 height: '100%',
                                 padding: '0 5vw 0 5vw',
+                                
+                              
                             }}
                         >
+                            <div style={{ fontSize: '20px', marginTop: '2vh' }}>Date</div>
+                            <Input style= {{borderRadius: '1vw'}} onChange={(event) => setDate(event.target.value)} />
+
                             <div style={{ fontSize: '20px', marginTop: '2vh' }}>Deed Name</div>
-                            <Input onChange={(event) => setName(event.target.value)} />
+                            <Input style= {{borderRadius: '1vw'}}  onChange={(event) => setDeedName(event.target.value)} />
+
                             <div style={{ fontSize: '20px', marginTop: '2vh' }}>
-                                Legal First Name
+                                Grantee Full name
                             </div>
-                            <Input onChange={(event) => setRole(event.target.value)} />
+                            <Input style= {{borderRadius: '1vw'}}  onChange={(event) => setGranteeName(event.target.value)} multiple />
+
                             <div style={{ fontSize: '20px', marginTop: '2vh' }}>
-                                Legal Last Name
+                                Grantee Phone Number
                             </div>
-                            <Input onChange={(event) => setRole(event.target.value)} multiple />
+                            <Input style= {{borderRadius: '1vw'}}  onChange={(event) => setGranteePhone(event.target.value)} />
+                            
+                            <div style={{ fontSize: '20px', marginTop: '2vh' }}>
+                                Address
+                            </div>
+                            <Input style= {{borderRadius: '1vw'}} onChange={(event) => setAddress(event.target.value)} />
+
                             <div style={{ fontSize: '20px', marginTop: '2vh' }}>
                                 Property Description
                             </div>
-                            <Input onChange={(event) => setMetamaskAddress(event.target.value)} />
+                            <TextArea style= {{borderRadius: '1vw'}}  onChange={(event) => setPropertyDescription(event.target.value)} />
+
+                            
+                        </div>
                         </div>
                         <div
                             style={{
@@ -81,6 +162,7 @@ function Form() {
                                 flexDirection: 'column',
                                 alignItems: 'center',
                                 padding: '',
+                                marginTop:'3vh'
                             }}
                         >
                             <div style={{ fontSize: '20px', marginBottom: '1vh' }}>
@@ -130,7 +212,7 @@ function Form() {
                             width: '200px',
                             height: '45px',
                             fontSize: '25px',
-                            marginTop: '20px',
+                          
                         }}
                     >
                         APPLY
